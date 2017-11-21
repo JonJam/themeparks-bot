fe9b0e5a783e65f772607059ea558e86a83b4ec7
Jonathan Harrison <j.harrison@altitudeangel.com>
2017-11-21 09:11:40 +0000

    WIP

--- src/bot/dialogs/parks.ts
+++ src/bot/dialogs/parks.ts
@@ -1,26 +1,41 @@
 import { CardAction, Library, Message, SuggestedActions } from "botbuilder";
-// import themeparks = require("themeparks");
+import themeparks = require("themeparks");
+
+// TODO Move to another module
+// TODO Figure out how to use Park from themeparks
+const parks = new Map<string, any>();
+
+// TODO Sort this.
+// TODO limit to some of the most popular ones
+themeparks.AllParks.forEach(Park => {
+  const park = new Park();
+
+  parks.set(park.Name, park);
+});
 
 const lib = new Library("parks");
 
 lib.dialog("whichPark", [
   function(session) {
+    // TODO refactor this.
+    const suggestedActions: CardAction[] = [];
+
+    parks.forEach((_val, key) => {
+      suggestedActions.push(CardAction.imBack(session, key, key));
+    });
+
     // https://docs.microsoft.com/en-us/bot-framework/nodejs/bot-builder-nodejs-send-suggested-actions
     const message = new Message(session)
       // TODO replace with strings
+      // TODO Update text to say some examples are:
       .text("Which park are you interested in?")
-      .suggestedActions(
-        SuggestedActions.create(session, [
-          CardAction.imBack(session, "productId=1&color=green", "Green"),
-          CardAction.imBack(session, "productId=1&color=blue", "Blue"),
-          CardAction.imBack(session, "productId=1&color=red", "Red")
-        ])
-      );
+      .suggestedActions(SuggestedActions.create(session, suggestedActions));
 
     session.send(message);
   },
   function(session, results) {
     // TODO handle a result which isn't expected.
+    // TODO Handle ambigious e.g. Magic Kingdom whether it is paris or orlando.
     session.endDialogWithResult(results);
   }
 ]);
