1e4be2c3f4a5cead6b0bbd245ed20cc892e1fded
Jonathan Harrison <jonjam1990@googlemail.com>
2017-11-30 22:07:40 +0000

    WIP

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,15 +1,15 @@
 import moment = require("moment-timezone");
 
+interface IAdditionalParkOperatingHours {
+  opening: moment.Moment;
+  closing: moment.Moment;
+  description: string;
+}
+
 export interface IParkOperatingHours {
   date: moment.Moment;
   opening: moment.Moment;
   closing: moment.Moment;
   isOpen: boolean;
   additionalHours?: IAdditionalParkOperatingHours[];
-}
-
-interface IAdditionalParkOperatingHours {
-  opening: moment.Moment;
-  closing: moment.Moment;
-  description: string;
 }
