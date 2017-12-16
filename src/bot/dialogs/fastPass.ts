9f2fab1b3a8d5dff615e5ce3644e8143596117f5
Jonathan Harrison <jonjam1990@googlemail.com>
2017-12-16 19:43:20 +0000

    WIP

--- src/bot/dialogs/fastPass.ts
+++ src/bot/dialogs/fastPass.ts
@@ -38,6 +38,94 @@ lib
     matches: "fastPass:all"
   });
 
-// TODO Specific ride
+// TODO Create intent
+// TODO implement below
+// TODO Remember to check fastpass on theme park
+
+// lib
+// .dialog("ride", [
+//   async (session, args, next) => {
+//     session.sendTyping();
+
+//     const rideStatusEntity: IEntity = EntityRecognizer.findEntity(
+//       args.intent.entities,
+//       "rideStatus"
+//     );
+//     const rideNameEntity: IEntity | null = EntityRecognizer.findEntity(
+//       args.intent.entities,
+//       "rideName"
+//     );
+
+//     session.dialogData.status = rideStatusEntity.entity;
+
+//     // Removing undefined since at this point it will be set.
+//     const park = getSelectedPark(session)!;
+
+//     const ridesInfo = await getRidesInfo(park);
+
+//     if (ridesInfo !== null) {
+//       const rideNames = ridesInfo.map(ri => ri.name);
+//       session.dialogData.ridesInfo = ridesInfo;
+
+//       let rideName: string | null = null;
+
+//       if (rideNameEntity !== null) {
+//         // LUIS found a ride name.
+//         rideName = getClosestMatch(rideNameEntity.entity, rideNames);
+//       }
+
+//       if (rideName !== null) {
+//         const dialogResult: IDialogResult<string> = {
+//           response: rideName
+//         };
+
+//         // Removing undefined as we do have a next step.
+//         next!(dialogResult);
+//       } else {
+//         const whichRideArgs: IWhichRideArgs = {
+//           rideNames
+//         };
+
+//         session.beginDialog("rides:whichRide", whichRideArgs);
+//       }
+//     } else {
+//       session.endDialog(strings.status.common.noData);
+//     }
+//   },
+//   (session, result: IDialogResult<string>) => {
+//     const parkName = result.response;
+
+//     const ridesInfo: IRideInfo[] = session.dialogData.ridesInfo;
+//     const status: RideStatus = session.dialogData.status;
+
+//     // Removing undefined as the park name comes from this list.
+//     const rideInfo = ridesInfo.find(ri => ri.name === parkName)!;
+
+//     let yesNo = "";
+
+//     if (status === RideStatus.Open) {
+//       yesNo = rideInfo.isRunning
+//         ? strings.status.ride.yes
+//         : strings.status.ride.no;
+//     } else {
+//       yesNo = !rideInfo.isRunning
+//         ? strings.status.ride.yes
+//         : strings.status.ride.no;
+//     }
+
+//     const message = format(
+//       strings.status.ride.message,
+//       yesNo,
+//       rideInfo.name,
+//       rideInfo.isRunning ? strings.status.ride.open : strings.status.ride.open
+//     );
+
+//     session.endDialog(message);
+//   }
+// ])
+// .triggerAction({
+//   // LUIS intent
+//   matches: "waitTimes:ride"
+// });
 
 export default lib;
