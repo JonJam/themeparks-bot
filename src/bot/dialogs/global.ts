c96ca7e3db3db65779fea5cddfe2826f32bfbee2
Jonathan Harrison <j.harrison@altitudeangel.com>
2018-01-09 08:28:29 +0000

    implemented switch park

--- src/bot/dialogs/global.ts
+++ src/bot/dialogs/global.ts
@@ -1,4 +1,5 @@
-import { Library, Session } from "botbuilder";
+import { IDialogResult, Library, Session } from "botbuilder";
+import { setSelectedPark } from "../data/userData";
 import strings from "../../strings";
 
 const lib = new Library("global");
@@ -13,9 +14,20 @@ lib
   });
 
 lib
-  .dialog("switchPark", (session: Session) => {
-    session.endDialog("switchPark");
-  })
+  .dialog("switchPark", [
+    // TODO See if can reduce duplication. Copied from default.
+    (session: Session) => {
+      session.beginDialog("parks:whichPark");
+    },
+    (session: Session, results: IDialogResult<string>) => {
+      // Removing undefined as we know that if reach here then a park has been selected.
+      const park = results.response!;
+
+      setSelectedPark(session, park);
+
+      session.beginDialog("parks:parkIntro");
+    }
+  ])
   .triggerAction({
     // LUIS intent
     matches: "global:switchPark"
