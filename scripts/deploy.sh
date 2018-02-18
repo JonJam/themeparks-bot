c21e14bf2cadacbef8c9e5c4c3ffaf3733055aa1
Jonathan Harrison <jonjam1990@googlemail.com>
2018-02-18 18:07:09 +0000

    Updating env variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,10 +10,10 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Follow the steps here to create a Service Principal: https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-create-service-principal-portal
-az login --service-principal -u $servicePrincipalId -p $servicePrincipalSecret --tenant $tenant 1> /dev/null
+az login --service-principal -u $SERVICE_PRINCIPAL_ID -p $SERVICE_PRINCIPAL_SECRET --tenant $TENANT 1> /dev/null
 
 echo "Getting credentials for cluster..."
-az aks get-credentials --name $clusterName --resource-group $resourceGroupName
+az aks get-credentials --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP_NAME
 
 echo "Installing release"
 helm upgrade --install "one-and-only" --set imageTag=0.0.$TRAVIS_BUILD_NUMBER,secret.luisAppUrl=$LUIS_APP_URL,secret.storageAccountName=$STORAGE_ACCOUNT_NAME,secret.storageAccountKey=$STORAGE_ACCOUNT_KEY,secret.microsoftAppId=$MICROSOFT_APP_ID,secret.microsoftAppPassword=$MICROSOFT_APP_PASSWORD ./themeparks-bot/
