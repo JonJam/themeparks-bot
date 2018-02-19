784f879cb391ab713a18d426587639dd4dbce856
JonJam <jonjam1990@googlemail.com>
2018-02-19 09:17:21 +0000

    Implemented caching

---
+++ src/services/cache.ts
@@ -0,0 +1,23 @@
+import { caching } from "cache-manager";
+import * as redisStore from "cache-manager-redis-store";
+import * as debug from "debug";
+import { redisHost, redisKey, redisPort } from "../settings";
+
+const log = debug("services:cache");
+
+// Configuring caching for themeparks. Documentation for options: https://github.com/NodeRedis/node_redis#rediscreateclient
+const redisCache = caching({
+  auth_pass: redisKey,
+  host: redisHost,
+  port: redisPort,
+  store: redisStore,
+  tls: { servername: redisHost }
+});
+
+const redisClient = redisCache.store.getClient();
+
+redisClient.on("error", (e: Error) => {
+  log(e);
+});
+
+export default redisCache;
