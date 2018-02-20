f09e91ecce77f8ffdf5d0a7de22b7c546517301d
JonJam <jonjam1990@googlemail.com>
2018-02-20 08:53:29 +0000

    Added init helm commands

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,6 +15,12 @@ az login --service-principal -u $SERVICE_PRINCIPAL_ID -p $SERVICE_PRINCIPAL_SECR
 echo "Getting credentials for cluster..."
 az aks get-credentials --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP_NAME
 
+echo "Initialising helm..."
+helm init --client-only --override storage=secret
+
+echo "Updating chart lists..."
+helm repo update
+
 echo "Updating chart dependencies..."
 helm dependency update ./themeparks-bot/
 
