0c70bed8a4339f6ba9b09f0f4a1565463118ae08
JonJam <jonjam1990@googlemail.com>
2018-07-02 08:49:47 +0100

    Updated dependencies

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -5,20 +5,25 @@ import { redisHost, redisKey, redisPort } from "../settings";
 
 const log = debug("services:cache");
 
-// Configuring caching for themeparks. Documentation for options: https://github.com/NodeRedis/node_redis#rediscreateclient
-const redisCache = caching({
-  auth_pass: redisKey,
-  host: redisHost,
-  port: redisPort,
-  store: redisStore
-  // If want to use TLS then uncomment below
-  // tls: { servername: redisHost }
-});
+let redisCache: any;
 
-const redisClient = redisCache.store.getClient();
+// If redisHost has not been defined, then do not use caching.
+if (redisHost) {
+  // Configuring caching for themeparks. Documentation for options: https://github.com/NodeRedis/node_redis#rediscreateclient
+  redisCache = caching({
+    auth_pass: redisKey,
+    host: redisHost,
+    port: redisPort,
+    store: redisStore
+    // If want to use TLS then uncomment below
+    // tls: { servername: redisHost }
+  });
 
-redisClient.on("error", (e: Error) => {
-  log(e);
-});
+  const redisClient = redisCache.store.getClient();
+
+  redisClient.on("error", (e: Error) => {
+    log(e);
+  });
+}
 
 export default redisCache;
